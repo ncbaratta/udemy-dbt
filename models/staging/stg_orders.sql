68d1265e7ef8bafd9c580cbd56b15ce600cba4ca
Nicole C. Baratta <ncbaratta@redhat.com>
2026-04-14 18:23:11 +0000

    seeds

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,10 +8,14 @@ o.ordersellingprice, o.ordercostprice,
 c.customername, c.segment, c.country, c.customerid,
 --from raw product
 p.category, p.productname, p.subcategory, p.productid,
-{{markup('ordersellingprice','ordercostprice')}} as markup
+{{markup('ordersellingprice','ordercostprice')}} as markup,
+--from seed
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c 
 on (o.customerid=c.customerid)
 left join {{ ref('raw_product') }} as p 
 on (o.productid=p.productid)
+left join {{ ref('delivery_team') }} as d 
+on (o.shipmode=d.shipmode)
 {{limit_data_in_dev('orderdate')}}

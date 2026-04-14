5cafec3d925700157b636d773ea8dec55a1a7ea9
Nicole C. Baratta <ncbaratta@redhat.com>
2026-04-14 18:17:36 +0000

    packages

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,5 @@
 select
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as sk_orders,
 --from raw orders
 o.orderid, o.orderdate, o.shipdate, o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,

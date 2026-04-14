56ac1b196950a331a81df3ea6680da67f66f0a82
Nicole C. Baratta <ncbaratta@redhat.com>
2026-04-14 18:11:57 +0000

    macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,9 +6,11 @@ o.ordersellingprice, o.ordercostprice,
 --from raw customers
 c.customername, c.segment, c.country, c.customerid,
 --from raw product
-p.category, p.productname, p.subcategory, p.productid
+p.category, p.productname, p.subcategory, p.productid,
+{{markup('ordersellingprice','ordercostprice')}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c 
 on (o.customerid=c.customerid)
 left join {{ ref('raw_product') }} as p 
 on (o.productid=p.productid)
+{{limit_data_in_dev('orderdate')}}

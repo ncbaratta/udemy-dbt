4d6862943bc7988121950aad5fc97fe30e3b1150
Nicole C. Baratta <ncbaratta@redhat.com>
2026-04-14 15:05:42 +0000

    developed reports

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,9 +4,9 @@ o.orderid, o.orderdate, o.shipdate, o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordersellingprice, o.ordercostprice,
 --from raw customers
-c.customername, c.segment, c.country,
+c.customername, c.segment, c.country, c.customerid,
 --from raw product
-p.category, p.productname, p.subcategory
+p.category, p.productname, p.subcategory, p.productid
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c 
 on (o.customerid=c.customerid)
